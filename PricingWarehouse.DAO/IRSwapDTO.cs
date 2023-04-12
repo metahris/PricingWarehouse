93a7199931e0a21b198c786cde130afa714fa64d
metahris <simotahri1@gmail.com>
2023-04-12 17:09:31 +0200

    add IR Swap DTO

---
+++ PricingWarehouse.DAO/IRSwapDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace PricingWarehouse.DAO
+{
+    public class IRSwapDTO
+    {
+        public string Currency { get; set; }  // Swap currency 
+        public double Notional { get; set; }
+        public double FixedRate { get; set; }  // Fixed rate 
+        public string FloatingRateReference { get; set; }  // Reference rate (e.g. "EURIBOR")
+        public double FloatingRateSpread { get; set; }  // Swap Spread 
+        public string DayCountConvention { get; set; }  
+        public int PaymentFrequencyMonths { get; set; }  // Payment frequency in months 
+        public DateTime ValuationDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double SwapValue { get; set; } 
+        
+    }
+}
