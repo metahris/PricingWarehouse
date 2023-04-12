b92430e3fee834df9d4ae487495adfe844c1f399
metahris <simotahri1@gmail.com>
2023-04-12 17:10:08 +0200

    add Swaption DTO

---
+++ PricingWarehouse.DAO/SwaptionDTO.cs
@@ -0,0 +1,62 @@
+﻿namespace PricingWarehouse.DAO
+{
+    public interface ISwaptionDTO 
+    {
+        string ProductType { get; }
+        string OptionType { get; set; }
+        string SettlementType { get; set; }
+        DateTime OptionEffectiveDate { get; set; }
+        DateTime OptionExpirationDate { get; set; }
+        DateTime OptionValuationDate { get; set; }
+        DateTime SwapStartDate { get; set; }
+        DateTime SwapEndDate { get; set; }
+        double OptionPrice { get; set; }
+        double StrikeRate { get; set; }
+        string FloatRateReference { get; set; }
+        string Currency { get; set; }
+        double NotionalAmount { get; set; }
+        string PricingModel { get; set; }
+        int PaymentFrequencyMonths { get; set; }
+        string DayCountConvention { get; set; }
+    }
+
+    public class EuropeanSwaptionDTO : ISwaptionDTO
+    {
+        public string ProductType { get { return "European Swaption"; } }
+        public string OptionType { get; set; }
+        public string SettlementType { get; set; }
+        public DateTime OptionEffectiveDate { get; set; }
+        public DateTime OptionExpirationDate { get; set; }
+        public DateTime OptionValuationDate { get; set; }
+        public DateTime SwapStartDate { get; set; }
+        public DateTime SwapEndDate { get; set; }
+        public double OptionPrice { get; set; }
+        public double StrikeRate { get; set; }
+        public string FloatRateReference { get; set; }
+        public string Currency { get; set; }
+        public double NotionalAmount { get; set; }
+        public string PricingModel { get; set; }
+        public int PaymentFrequencyMonths { get; set; }
+        public string DayCountConvention { get; set; }  
+    }
+
+    public class BermudeanSwaptionDTO : ISwaptionDTO
+    {
+        public string ProductType { get { return "Bermudean Swaption"; } }
+        public string OptionType { get; set; }
+        public string SettlementType { get; set; }
+        public DateTime OptionEffectiveDate { get; set; }
+        public DateTime OptionExpirationDate { get; set; }
+        public DateTime OptionValuationDate { get; set; }
+        public DateTime SwapStartDate { get; set; }
+        public DateTime SwapEndDate { get; set; }
+        public double OptionPrice { get; set; }
+        public double StrikeRate { get; set; }
+        public string FloatRateReference { get; set; }
+        public string Currency { get; set; }
+        public double NotionalAmount { get; set; }
+        public string PricingModel { get; set; }
+        public int PaymentFrequencyMonths { get; set; }
+        public string DayCountConvention { get; set; }
+    }
+}
