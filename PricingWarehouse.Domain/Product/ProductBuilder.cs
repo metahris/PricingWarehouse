01d70d546ba62c7bc54f7c3f66e65550c9aba872
metahris <simotahri1@gmail.com>
2023-04-13 10:42:32 +0200

    add product builder

---
+++ PricingWarehouse.Domain/Product/ProductBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PricingWarehouse.Domain.Product
+{
+    public interface IProductBuilder
+    {
+        IProduct Build();
+    }
+}
