b7cf85b47aa0d17e7dba02663305debe9ffee2c1
metahris <simotahri1@gmail.com>
2023-04-13 16:17:27 +0200

    delete unused usings

--- PricingWarehouse.Domain/MarketData/VolatilityPoint.cs
+++ PricingWarehouse.Domain/MarketData/VolatilityPoint.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PricingWarehouse.Domain.MarketData
+﻿namespace PricingWarehouse.Domain.MarketData
 {
     public interface IVolatilityPoint
     {
