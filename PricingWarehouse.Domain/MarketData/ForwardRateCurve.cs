8cace1d4538aba526e1f4963dec43847ef9dd58b
metahris <simotahri1@gmail.com>
2023-04-13 16:19:23 +0200

    Add ForwardRateCurve

---
+++ PricingWarehouse.Domain/MarketData/ForwardRateCurve.cs
@@ -0,0 +1,12 @@
+﻿namespace PricingWarehouse.Domain.MarketData
+{
+    public class ForwardRateCurve:IRatesCurve
+    {
+        public string CurveType { get { return "ForwardRateCurve"; } }
+        public string Currency { get; private set; }
+        public DateTime CurveDate { get; private set; }
+        public string InterpolationType { get; private set; }
+        public string ExtrapolationType { get; private set; }
+        public List<CurveNode> Nodes { get; private set; }
+    }
+}
