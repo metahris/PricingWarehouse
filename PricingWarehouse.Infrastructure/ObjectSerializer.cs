02ad964e0cb3391d2cbae66abbeef4c079f5674d
metahris <simotahri1@gmail.com>
2023-04-13 16:14:56 +0200

    Infra : Add ObjectSerializer

---
+++ PricingWarehouse.Infrastructure/ObjectSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+
+namespace OptionPricer.Infrastructure
+{
+
+    public interface IObjectSerializer
+    {
+        string Serialize<T>(T obj);
+        T Deserialize<T>(string obj);
+    }
+    public class ObjectSerializer : IObjectSerializer
+    {
+        public string Serialize<T>(T obj)
+        {
+            try
+            {
+                return JsonSerializer.Serialize(obj);
+            }
+            catch (Exception exc)
+            {
+                throw new Exception($"Failed to serialize object of type {typeof(T).FullName}", exc);
+            }
+        }
+        public T Deserialize<T>(string obj)
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<T>(obj);
+            }
+            catch (Exception exc)
+            {
+
+                throw new Exception($"Failed to deserialize object of type {typeof(T).FullName}", exc);
+            }
+        }
+    }
+}
