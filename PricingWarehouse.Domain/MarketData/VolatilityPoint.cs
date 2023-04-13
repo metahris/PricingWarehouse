7f0a97fe690c2ff6c031b715c722ed7a95c9fbb6
metahris <simotahri1@gmail.com>
2023-04-13 16:15:53 +0200

    add volatility

---
+++ PricingWarehouse.Domain/MarketData/VolatilityPoint.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PricingWarehouse.Domain.MarketData
+{
+    public interface IVolatilityPoint
+    {
+        string Maturity { get; }
+        double Strike { get; }
+        double Value { get; }
+    }
+    public class VolatilityCubePoint: IVolatilityPoint
+    {
+        public string Maturity { get; private set; }
+        public string Tenor { get; private set; }
+        public double Strike { get; private set; }
+        public double Value { get; private set; }
+        public VolatilityCubePoint(string maturity, string tenor, double strike, double value)
+        {
+            Maturity = maturity;
+            Tenor = tenor;
+            Strike = strike;
+            Value = value;
+
+        }
+    }
+    public class VolatilitySurfacePoint:IVolatilityPoint
+    {
+        public string Maturity { get; private set; }
+        public double Strike { get; private set; }
+        public double Value { get; private set; }
+        public VolatilitySurfacePoint(string maturity, double strike, double value)
+        {
+            Maturity = maturity;
+            Strike = strike;
+            Value = value;
+        }
+    }
+}
