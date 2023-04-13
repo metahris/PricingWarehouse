75c27cb6151ead7250c2008c9b178cf430dec7f4
metahris <simotahri1@gmail.com>
2023-04-13 16:18:49 +0200

    Add IRatesCurve interface

---
+++ PricingWarehouse.Domain/MarketData/CurveNode.cs
@@ -0,0 +1,13 @@
+﻿namespace PricingWarehouse.Domain.MarketData
+{
+    public class CurveNode
+    {
+        public string Tenor { get; private set; }
+        public double Value { get; private set; }
+        public CurveNode(string tenor, double value)
+        {
+            Tenor = tenor;
+            Value = value;
+        }
+    }
+}
---
+++ PricingWarehouse.Domain/MarketData/IRatesCurve.cs
@@ -0,0 +1,12 @@
+﻿namespace PricingWarehouse.Domain.MarketData
+{
+    public interface IRatesCurve
+    {
+        string CurveType { get;}
+        string Currency { get;}
+        DateTime CurveDate { get;}
+        string InterpolationType { get;}
+        string ExtrapolationType { get;}
+        List<CurveNode> Nodes { get;}
+    }
+}
