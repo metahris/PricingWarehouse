d9adbeb7e69a8dc500aee96309bbc8898f8c04be
metahris <simotahri1@gmail.com>
2023-04-13 16:19:48 +0200

    Add MarketData

---
+++ PricingWarehouse.Domain/MarketData/MarketData.cs
@@ -0,0 +1,35 @@
+﻿using OptionPricer.Infrastructure;
+
+namespace PricingWarehouse.Domain.MarketData
+{
+    public interface IMarketData
+    {
+        string Serialize(IObjectSerializer objectSerializer);
+        MarketData Deserialize(string json, IObjectSerializer objectSerializer);
+    }
+    public class MarketData
+    {
+        public DateTime MarketDate { get; private set; }
+        public DiscountCurve DiscountCurve { get; private set; }
+        public ForwardRateCurve ForwardRatesCurve { get; private set; }
+        public SpotRateCurve SpotRateCurve { get; private set; }
+        public List<IVolatilityPoint> Volatilities { get; private set; }
+        public MarketData(DateTime marketDate, ForwardRateCurve forwardRatesCurve, DiscountCurve discountCurve, SpotRateCurve spotRateCurve, List<IVolatilityPoint> volatilities)
+        {
+            MarketDate = marketDate;
+            ForwardRatesCurve = forwardRatesCurve;
+            DiscountCurve = discountCurve;
+            SpotRateCurve = spotRateCurve;
+            Volatilities = volatilities;
+        }
+        public string Serialize(IObjectSerializer objectSerializer)
+        {
+            return objectSerializer.Serialize(this);
+        }
+
+        public static MarketData Deserialize(string json, IObjectSerializer objectSerializer)
+        {
+            return objectSerializer.Deserialize<MarketData>(json);
+        }
+    }
+}
