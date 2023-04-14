c8695237ed0f03da390951572c8d7622fd810837
metahris <simotahri1@gmail.com>
2023-04-14 12:58:56 +0200

    Add IPriceable interface

---
+++ PricingWarehouse.Domain/Priceable/IPriceable.cs
@@ -0,0 +1,11 @@
+﻿using PricingWarehouse.Domain.Product;
+using PricingWarehouse.Domain.MarketData;
+namespace PricingWarehouse.Domain.Priceable
+{
+    public interface IPriceable
+    {
+        List<IProduct> Products { get; }
+        IMarketData MarketData { get; }
+
+    }
+}
