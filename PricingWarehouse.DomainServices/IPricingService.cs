1de0c0cdc7f5e5811562b2f7ba295e6865701cac
metahris <simotahri1@gmail.com>
2023-04-14 10:32:56 +0200

    Add DomainServices

---
+++ PricingWarehouse.DomainServices/IPricingService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using PricingWarehouse.Domain.Product;
+
+
+namespace PricingWarehouse.DomainServices
+{
+    public interface IPricingService
+    {
+        double Price(IProduct product);
+
+    }
+}
