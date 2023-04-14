5a32b241c3300bd534594ce7039363aa2f96969d
metahris <simotahri1@gmail.com>
2023-04-14 10:07:29 +0200

    Add dependency injector

---
+++ PricingWarehouse.Infrastructure/DependencyInjector.cs
@@ -0,0 +1,37 @@
+﻿using DryIoc;
+namespace PricingWarehouse.Infrastructure
+{
+    public interface IDependencyInjector
+    {
+        void Register<TInterface, TImplementation>() where TImplementation : TInterface;
+        void Register<TInterface, TImplementation>(string key) where TImplementation : TInterface;
+        T Resolve<T>();
+        T Resolve<T>(string key);
+    }
+    public class DependencyInjector : IDependencyInjector
+    {
+        private readonly IContainer container;
+
+        public DependencyInjector()
+        {
+            container = new Container();
+        }
+
+        public void Register<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            container.Register<TInterface, TImplementation>();
+        }
+        public void Register<TInterface, TImplementation>(string key) where TImplementation : TInterface
+        {
+            container.Register<TInterface, TImplementation>(serviceKey: key);
+        }
+        public T Resolve<T>()
+        {
+            return container.Resolve<T>();
+        }
+        public T Resolve<T>(string key)
+        {
+            return container.Resolve<T>(serviceKey: key);
+        }
+    }
+}
