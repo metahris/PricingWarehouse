59445f644657b3fdc892e43231830b8497ecc7d4
metahris <simotahri1@gmail.com>
2023-04-16 20:23:10 +0200

    add OptionPriceable

---
+++ PricingWarehouse.Domain/Priceable/OptionPriceable.cs
@@ -0,0 +1,13 @@
+﻿namespace PricingWarehouse.Domain
+{
+    public class OptionPriceable:IPriceable<IOption>
+    {
+        public IList<IOption> Products { get; private set; }
+        public IMarketData MarketData { get; private set; }
+        public OptionPriceable(IList<IOption> options, IMarketData marketData)
+        {
+            Products = options;
+            MarketData = marketData;
+        }
+    }
+}
