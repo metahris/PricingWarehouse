135f020c8e2fd11846535f2d4f5ce14ef2fa32d3
metahris <simotahri1@gmail.com>
2023-04-16 20:26:24 +0200

    add OptionService

---
+++ PricingWarehouse.Service/OptionService.cs
@@ -0,0 +1,34 @@
+﻿using PricingWarehouse.Domain;
+using PricingWarehouse.DomainServices;
+using PricingWarehouse.Infrastructure;
+using PricingWarehouse.Repository;
+
+namespace PricingWarehouse.Service
+{
+    public class OptionService: IProductService<IOption>
+    {
+        private readonly IProductRepository<IOption> productRepository;
+
+        public OptionService(IProductRepository<IOption> productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        public int PersistProduct(IOption option)
+        {
+            return this.productRepository.InsertProduct(option);
+        }
+        public IList<IOption> Price(IPriceable<IOption> priceable, IDependencyInjector dependencyInjector)
+        {
+            var marketData = priceable.MarketData;
+            var pricingResults = new List<IOption>();
+            foreach (var option in priceable.Products)
+            {
+                var pricingService = dependencyInjector.Resolve<IPricingService<IProduct>>(option.PricingModel.ToString());
+                double price = pricingService.Price(option, marketData);
+                option.SetPrice(price);
+                pricingResults.Add(option);
+            }         
+            return pricingResults;
+        }
+    }
+}
