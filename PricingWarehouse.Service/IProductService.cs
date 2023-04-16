bf032277c25a0a7f111f859a72375d5617981c2b
metahris <simotahri1@gmail.com>
2023-04-16 20:25:59 +0200

    add product Service

---
+++ PricingWarehouse.Service/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using PricingWarehouse.Domain;
+using PricingWarehouse.Infrastructure;
+
+namespace PricingWarehouse.Service
+{
+    public interface IProductService<T> where T: IProduct
+    {
+        int PersistProduct(T product);
+        IList<T> Price(IPriceable<T> priceable, IDependencyInjector dependencyInjector);
+    }
+}
