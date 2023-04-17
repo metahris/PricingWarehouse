10c6451b5e560b4e67bcb1bce0fbcd487cd078be
metahris <simotahri1@gmail.com>
2023-04-17 14:21:57 +0200

    add NetMQ service

---
+++ PricingWarehouse.NETMQService/NETMQOptionPricing.cs
@@ -0,0 +1,29 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using PricingWarehouse.Domain;
+using PricingWarehouse.Infrastructure;
+using PricingWarehouse.NETMQService;
+using PricingWarehouse.Service;
+
+using (var responseSocket = new ResponseSocket("@tcp://*:5555"))
+{
+    var dependencyInjector = new DependencyInjector();
+    NETMQServiceRegistration.Build(dependencyInjector);
+
+    var objectSerializer = dependencyInjector.Resolve<IObjectSerializer>();
+    var optionService = dependencyInjector.Resolve<IProductService<IOption>>();
+
+    while (true)
+    {
+        Console.WriteLine("service up and running");
+        var message = responseSocket.ReceiveFrameString();
+        Console.WriteLine($"service received: {message}");
+
+        var priceable = objectSerializer.Deserialize<OptionPriceable>(message);
+        var pricingResults = optionService.Price(priceable, dependencyInjector);
+
+        var response = objectSerializer.Serialize(pricingResults);
+        responseSocket.SendFrame(response);
+    }
+
+}
---
+++ PricingWarehouse.NETMQService/NETMQServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using PricingWarehouse.Domain;
+using PricingWarehouse.DomainServices;
+using PricingWarehouse.Infrastructure;
+using PricingWarehouse.Repository;
+using PricingWarehouse.Service;
+
+namespace PricingWarehouse.NETMQService
+{
+    public class NETMQServiceRegistration
+    {
+        public static void Build(IDependencyInjector dependencyInjector)
+        {
+            dependencyInjector.Register<IObjectSerializer, ObjectSerializer>();
+            dependencyInjector.Register<IOption, EuropeanSwaption>();
+            dependencyInjector.Register<IProductRepository<EuropeanSwaption>, EuropeanSwaptionRepository>();
+            dependencyInjector.Register<IProductService<IOption>, OptionService>();
+            dependencyInjector.Register<IPricingService<IOption>, SabrPricingService>(PricingModel.Sabr.ToString());
+            dependencyInjector.Register<IPricingService<IOption>, HestonPricingService>(PricingModel.Heston.ToString());
+            dependencyInjector.Register<IPricingService<IOption>, HjmPricingService>(PricingModel.Hjm.ToString());
+            dependencyInjector.Register<IProductBuilder<EuropeanSwaption>, EuropeanSwaptionBuilder>();
+
+        }
+
+    }
+}
